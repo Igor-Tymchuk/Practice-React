305d93676aec99237100237feb01571663d07832
Anastasiia Bulba <fedinanastya@gmail.com>
2024-10-24 21:02:19 +0300

    bulba

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import ProductCard from "./components/ProductCard/ProductCard";
+import products from "../src/assets/productCard.json";
+import "modern-normalize";
 
 function App() {
-
   return (
     <>
-<Header/>
-<Footer/>
+      <Header />
+      <ProductCard products={products} />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
